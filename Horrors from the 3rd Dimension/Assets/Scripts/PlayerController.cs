d03ee9ae6960ead3e36189b6b3ef0d9b8816e91c
willwill96 <wbw4sv@virginia.edu>
2017-09-15 00:17:33 -0400

    Finally fixed .gitignore

---
+++ Horrors from the 3rd Dimension/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        checkInput();
+        
+    }
+    void checkInput()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement.x += 10.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement.x -= 10.0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement.z += 10.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement.z -= 10.0f;
+        }
+        movement.Normalize();
+        transform.Translate(movement*Time.deltaTime*10, Space.World);
+        Vector3 rotation = new Vector3(0.0f, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement.y -= 120.0f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement.y += 120.0f;
+        }
+        transform.Rotate(movement * Time.deltaTime);
+    }
+}
