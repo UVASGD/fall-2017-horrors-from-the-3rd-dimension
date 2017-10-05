94a2b448b19548a04c2f30b30adc7e5a73c2e4a9
walkianm <walkianm@gmail.com>
2017-10-05 18:21:47 -0400

    Made things move and not move.
    Yay.

---
+++ Horrors from the 3rd Dimension/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is rudimentary and will probably not be used because I don't really know what I'm doing but hopefully I learn something.
+
+public class EnemyMover : MonoBehaviour {
+    private Rigidbody rb;
+    private Vector3 translate = new Vector3();
+    private Vector3 direction = new Vector3();
+    private float force;
+    private float targetAngle;
+
+    public float cornerPos;
+    public int numCorners;
+    public float maxSpeed;
+    public float drag;
+    
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        force = (1 / (1 - drag) - 1) * maxSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        locateTarget();
+        moveObject();
+        rotateObject();
+        applyDrag();
+        keepObjectOnPlane();
+	}
+
+    void locateTarget()
+    {
+        direction = GameObject.Find("Player").transform.position - transform.position;
+        direction.Normalize();
+        targetAngle = Mathf.Acos(direction[0]);//in radians
+        if (direction[2] < 0)
+        {
+            targetAngle *= -1;
+            targetAngle += 2 * Mathf.PI;
+        }
+        targetAngle /= Mathf.PI / 180;
+        targetAngle = 360 - targetAngle;
+        //print(targetAngle);
+    }
+
+    void rotateObject()
+    {
+        rb.angularVelocity = new Vector3(0, 0.0005f * findMinDif() / Time.deltaTime, 0);//controls rotation
+    }
+
+    float findMinDif()//this mess should work for any shape with evenly spaced corners
+    {
+        targetAngle -= cornerPos;
+        while (Mathf.Abs(targetAngle) > 360 / numCorners / 2)
+        {
+            targetAngle -= 360 / numCorners;
+        }
+        targetAngle -= transform.eulerAngles.y;
+        while (targetAngle < -180 / numCorners)
+        {
+            targetAngle += 360 / numCorners;
+        }
+           
+        return targetAngle;
+    }
+
+    void moveObject()
+    {
+        translate += direction;
+        rb.velocity = force * translate / Time.deltaTime;
+    }
+
+    void applyDrag()
+    {
+        translate *= (1 - drag);
+    }
+
+    void keepObjectOnPlane()
+    {
+        transform.position = (new Vector3(transform.position.x, 0, transform.position.z));//keeps object on plane
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);//keeps x and z rotation 0
+    }
+}
--- Horrors from the 3rd Dimension/Assets/Scripts/PlayerController.cs
+++ Horrors from the 3rd Dimension/Assets/Scripts/PlayerController.cs
@@ -4,46 +4,170 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
-	// Use this for initialization
-	void Start () {
+    private float spin;
+    private Vector3 translate = new Vector3(0.0f, 0.0f, 0.0f);
+    private float force;
+    private float torque;
 
+    public float spinDrag;
+    public float spinSpeed;
+    public float maxSpeed;
+    public float drag; //set drag to 1 for minimal acceleration, 0 for most acceleration
+    public bool dirMovement;
+    
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        spin = 0;
+
+        if(spinDrag == 1)//prevents divide by 0 error
+        {
+            spinDrag -= 0.00001f;
+        }
+        if(drag == 1)
+        {
+            drag -= 0.00001f;
+        }
+
+        force = (1 / (1 - drag) - 1) * maxSpeed;
+        torque = (1 / (1 - spinDrag) - 1) * spinSpeed;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         checkInput();
-        
+        applyDrag();
+        moveObject();
+        rotateObject();
+        keepObjectOnPlane();
+
+        BoxCollider b = this.GetComponent<BoxCollider>();//not used yet
     }
+
+    void applyDrag()
+    {
+        translate *= (1 - drag);
+        spin *= (1 - spinDrag);
+    }
+
+    void keepObjectOnPlane()
+    {
+        transform.position = (new Vector3(transform.position.x, 0, transform.position.z));//keeps player on plane
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);//keeps x and z rotation 0
+    }
+
+    void rotateObject()
+    {
+        rb.angularVelocity = new Vector3(0, spin / Time.deltaTime, 0);//controls rotation
+    }
+
+    void moveObject()
+    {
+        if (!dirMovement)
+        {
+            rb.velocity = translate / Time.deltaTime;
+        }
+        else
+        {
+            convertMotion();//changes the up/down motion into motion relative to the object itself, then applies it
+        }
+    }
+
+    void convertMotion()
+    {
+        float angle = transform.eulerAngles.y;
+        angle = angle / 180 * Mathf.PI;
+        angle += Mathf.PI/4;
+        angle %= 2 * Mathf.PI;
+
+        Vector3 move = new Vector3(Mathf.Sin(angle),0,Mathf.Cos(angle));
+        rb.velocity = move*translate[2]/Time.deltaTime;
+    }
+
     void checkInput()
     {
-        Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+        moveX();
+        moveZ();
+        rotate();
+    }
+
+    void moveX()
+    {
+        if (!dirMovement)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))//if moving in more than one direction, slows movement accordingly
+                {
+                    translate[0] -= force * 0.7071067812f;
+                }
+                else
+                {
+                    translate[0] -= force;
+                }
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+                {
+                    translate[0] += force * 0.7071067812f;
+                }
+                else
+                {
+                    translate[0] += force;
+                }
+            }
+        }
+    }
+
+    void moveZ()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            movement.x += 10.0f;
+            if (!dirMovement && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+            {
+                translate[2] += force * 0.7071067812f;
+            }
+            else
+            {
+                translate[2] += force;
+            }
         }
         if (Input.GetKey(KeyCode.S))
         {
-            movement.x -= 10.0f;
+            if (!dirMovement && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+            {
+                translate[2] -= force * 0.7071067812f;
+            }
+            else
+            {
+                translate[2] -= force;
+            }
         }
-        if (Input.GetKey(KeyCode.A))
+    }
+
+    void rotate()
+    {
+        if (!dirMovement)
         {
-            movement.z += 10.0f;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                spin -= torque;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                spin += torque;
+            }
         }
-        if (Input.GetKey(KeyCode.D))
+        else
         {
-            movement.z -= 10.0f;
+            if (Input.GetKey(KeyCode.A))
+            {
+                spin -= torque;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                spin += torque;
+            }
         }
-        movement.Normalize();
-        transform.Translate(movement*Time.deltaTime*10, Space.World);
-        Vector3 rotation = new Vector3(0.0f, 0.0f, 0.0f);
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            movement.y -= 120.0f;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            movement.y += 120.0f;
-        }
-        transform.Rotate(movement * Time.deltaTime);
     }
 }
