1cab039bede9e0a870c7afffc65cf9d58968893a
MAKtheUnknown <mak@rehost.com>
2017-12-05 20:03:28 -0800

    Made a Main Menue

---
+++ Horrors from the 3rd Dimension/Assets/StartButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+
+	TextMesh text;
+
+	// Use this for initialization
+	void Start () 
+	{
+		text = GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver()
+	{
+		text.text = anagramatic (text.text);
+		print ("tstytst");
+	}
+
+	void OnMoustUp()
+	{
+
+	}
+
+	string anagramatic(string original)
+	{
+		ArrayList ls = new ArrayList();
+		string ret = "";
+		System.Random rand = new System.Random ();
+
+		foreach (char c in original) 
+		{
+			ls.Add (c);
+		}
+
+		while ((ls.Count == 0) == false) 
+		{
+			int i = rand.Next(0, ls.Count);
+			ret = ret + ls [i];
+			ls.RemoveAt (i);
+		}
+		return ret;
+	}
+}
